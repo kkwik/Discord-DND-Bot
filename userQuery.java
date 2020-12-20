ec61d47cfd9894685195684d0a593f2a672c325c
kkwik <kevinkwik6@gmail.com>
2020-12-20 12:44:08 -0800

    Updated documentation

--- userQuery.java
+++ userQuery.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 //Stores the latest query of of a user
 public class userQuery<E> {
-    private final User author;
-    private final ArrayList<E> queryResults;
-    private final queryType type;
+    private final User author;                  //The user who created this query. Each user can have at most one userquery
+    private final ArrayList<E> queryResults;    //The list of results from the query
+    private final queryType type;               //The type of query it was. Currently supports only feats and spells
 
     
     protected userQuery(final User author, final ArrayList<E> queryResults, final queryType type)
